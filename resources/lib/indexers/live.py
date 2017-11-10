076b5d914febe63b4d117693b879fd9e47cf4ae0
Unknown <twilight@freemail.gr>
2017-11-10 10:23:25 +0200

    AliveGR 1.5.5

--- resources/lib/indexers/live.py
+++ resources/lib/indexers/live.py
@@ -85,7 +85,8 @@ class Main:
 
         log_notice('Number of live channels available: ' + str(len(self.list)) + ', cached for 4 hours')
 
-        if control.setSetting('debug') == 'true': log_debug('Live list uncached' + repr(self.list))
+        if control.setting('debug') == 'true':
+            log_debug('Live list uncached' + repr(self.list))
 
         return self.list, self.groups, updated
 
@@ -120,7 +121,7 @@ class Main:
         if self.list is None:
             log_error('Live channels list did not load successfully')
             return
-        elif control.setSetting('debug') == 'true':
+        elif control.setting('debug') == 'true':
             log_debug('Caching was successful, list of channels ~ ' + repr(self.list))
         else:
             log_notice('Cached live channels available: ' + str(len(self.list)))
